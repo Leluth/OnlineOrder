9cb937f38d135ca7b9c9b8c2a05eef7a0f1dd3df
Shaoshuai-Xu <xu.shaosh@northeastern.edu>
2021-09-25 16:02:23 -0700

    [Update]Update Servlet and Add entity

--- src/main/java/com/ssxu/OnlineOrder/HelloServlet.java
+++ src/main/java/com/ssxu/OnlineOrder/HelloServlet.java
@@ -1,8 +1,15 @@
 package com.ssxu.OnlineOrder;
 
-import java.io.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssxu.OnlineOrder.entity.Customer;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
@@ -14,14 +21,38 @@ public class HelloServlet extends HttpServlet {
     }
 
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+        ObjectMapper mapper = new ObjectMapper();
+        Customer customer = new Customer();
+        customer.setEmail("sun@laioffer.com");
+        customer.setPassword("123456");
+        customer.setFirstName("rick");
+        customer.setLastName("sun");
+        customer.setEnabled(true);
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+        response.getWriter().print(mapper.writeValueAsString(customer));
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // Read customer information from request body
+        JSONObject jsonRequest = new JSONObject(IOUtils.toString(request.getReader()));
+        String email = jsonRequest.getString("email");
+        String firstName = jsonRequest.getString("first_name");
+        String lastName = jsonRequest.getString("last_name");
+        int age = jsonRequest.getInt("age");
+        // Print customer information to IDE console
+        System.out.println("Email is: " + email);
+        System.out.println("First name is: " + firstName);
+        System.out.println("Last name is: " + lastName);
+        System.out.println("Age is: " + age);
+        // Return status = ok as response body to the client
+        response.setContentType("application/json");
+        JSONObject jsonResponse = new JSONObject();
+        jsonResponse.put("status", "ok");
+        response.getWriter().print(jsonResponse);
     }
 
     @Override
---
+++ src/main/java/com/ssxu/OnlineOrder/entity/Customer.java
@@ -0,0 +1,55 @@
+package com.ssxu.OnlineOrder.entity;
+
+/**
+ * @author Shaoshuai Xu
+ * @version 1.0
+ * @description: Customer
+ * @date 2021/9/25 15:56
+ */
+public class Customer {
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private boolean enabled;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
