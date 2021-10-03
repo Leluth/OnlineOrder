210b2229e549589e8d7f40cc20783b7f0e23e0fb
Shaoshuai-Xu <xu.shaosh@northeastern.edu>
2021-10-03 16:00:59 -0700

    [Update]Update dataSource

--- src/main/java/com/ssxu/onlineOrder/ApplicationConfig.java
+++ src/main/java/com/ssxu/onlineOrder/ApplicationConfig.java
@@ -29,15 +29,14 @@ public class ApplicationConfig {
 
     @Bean(name = "dataSource")
     public DataSource dataSource() {
-        String RDS_INSTANCE = DataSourceConfig.RDS_INSTANCE;
-        String USERNAME = DataSourceConfig.USERNAME;
-        String PASSWORD = DataSourceConfig.PASSWORD;
+        String rdsInstance = DataSourceConfig.RDS_INSTANCE;
+        String username = DataSourceConfig.USERNAME;
+        String password = DataSourceConfig.PASSWORD;
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://" + RDS_INSTANCE + ":3306/onlineOrder?createDatabaseIfNotExist=true&serverTimezone=UTC");
-        dataSource.setUsername(USERNAME);
-        dataSource.setPassword(PASSWORD);
-
+        dataSource.setUrl("jdbc:mysql://" + rdsInstance + ":3306/onlineOrder?createDatabaseIfNotExist=true&serverTimezone=UTC");
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
         return dataSource;
     }
 
@@ -48,5 +47,4 @@ public class ApplicationConfig {
         hibernateProperties.setProperty("hibernate.show_sql", "true");
         return hibernateProperties;
     }
-
 }
